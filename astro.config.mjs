40677d01abfbb8990bdcb53cbc16f5bc745eb2ba
Quentin Anvo <quentin.anvo@gmail.com>
2024-11-24 07:50:02 +0100

    fix: correction config déploiement astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://quentin.anvo.github.io',
-  })
+  site: 'https://q-anvo.github.io',
+  outDir: 'dist',
+  base: '/',
+  publicDir: 'ressources',
+});
